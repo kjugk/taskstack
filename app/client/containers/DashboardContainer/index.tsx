b222604daf24475e0c21ecbd14f6f83e272fdacc
Koji Uegaki <kjugkjk@gmail.com>
2018-07-05 23:20:01 +0900

    Create Dashboard.

--- app/client/containers/DashboardContainer/index.tsx
+++ app/client/containers/DashboardContainer/index.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import { TasklistListContainer } from '../TasklistListContainer';
+
+const DashBoard = styled.div`
+  height: 100%;
+`;
+
+const Left = styled.div`
+  width: 200px;
+  height: 100%;
+  float: left;
+`;
+
+const Right = styled.div`
+  height: 100%;
+  overflow: hidden;
+`;
 
 class DashboardContainer extends React.Component {
   render() {
     return (
-      <div>
-      </div>
-    )
+      <DashBoard>
+        <Left>
+          <TasklistListContainer />
+        </Left>
+        <Right />
+      </DashBoard>
+    );
   }
 }
 
---
+++ app/client/containers/TasklistListContainer/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class TasklistListContainer extends React.Component {
+  render() {
+    return <div>aaa</div>;
+  }
+}
+
+export { TasklistListContainer };
